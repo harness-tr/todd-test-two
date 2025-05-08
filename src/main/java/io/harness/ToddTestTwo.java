8b4b4cc491c5e719cb048dd0c7a4c62425e95002
parson-harness <>
2025-05-08 17:25:55 +0000

    Project init

---
+++ src/main/java/io/harness/ToddTestTwo.java
@@ -0,0 +1,11 @@
+package io.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToddTestTwo {
+    public static void main(String[] args) {
+        SpringApplication.run(ToddTestTwo.class, args);
+    }
+}
